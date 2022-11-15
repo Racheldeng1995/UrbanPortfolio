1183dcc9ab64feec41d7aa8660f8373cd9bd3d64
Jingsi <gongjingsi@hotmail.com>
2022-11-15 15:23:33 -0500

    adjust color

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="w-100 mt-auto bg-img-2 p-4">
       <div className='flex-row justify-center my-2 mx-2'>
         <a href="https://github.com/Racheldeng1995/"><i className='mx-2 fa fa-facebook'></i></a>
         <a href="https://github.com/Racheldeng1995/"><i className='mx-2 fa fa-twitter'></i></a>
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,7 +10,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="bg-img-1 mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>Urban Portfolio</h1>
