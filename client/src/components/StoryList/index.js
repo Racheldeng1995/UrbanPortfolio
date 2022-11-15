8a4ff7e1d8183a68345b95fff2a1d7c09b46337c
Jingsi <gongjingsi@hotmail.com>
2022-11-15 16:32:25 -0500

    adjust story header style

--- client/src/components/StoryList/index.js
+++ client/src/components/StoryList/index.js
@@ -14,19 +14,21 @@ const StoryList = ({ stories, title }) => {
           stories.map(story => (
             <div key={story._id} className="col-3 card ml-1 mr-1 mb-2 mt-2 p-1">
               <div className="card-header p-1">
+                <h4 className="text-center p-2">{story.storyTitle}</h4>
+                <h5 className="text-center">story posted on</h5>
+                <h5 className="text-center">{story.createdAt}</h5>
                 <Link
                   to={`/profile/${story.username}`}
                   style={{ fontWeight: 700 }}
                   // className="text-light"
                 >
-                <div className="text-center">{story.username}</div>
+                <h5 className="text-right pr-2 text-italic p-1 text-extralight">By {story.username}</h5>
                 </Link>{' '}
-                <h5 className="text-center">story posted on</h5>
-                <h5 className="text-center">{story.createdAt}</h5>
               </div>
               <div className="card-body text-justify p-2">
+
                 <Link to={`/story/${story._id}`}>
-                  <p>{story.storyTitle}</p>
+                  
                   <div className="mb-0">
                     <p className="text-center">Comments: {story.commentCount}  Likes: {story.likes.length? (story.likes.length): (0) } </p>
                     <h6>Click to{' '}
