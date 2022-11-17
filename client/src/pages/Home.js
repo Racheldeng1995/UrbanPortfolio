950dfc3b952db7a317f567e1468c140528746d52
MertCetin <mert.cetnn@gmail.com>
2022-11-17 15:24:44 -0500

    cards css edited

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import StoryList from '../components/StoryList';
-import StoryForm from '../components/StoryForm';
-import FriendList from '../components/FriendList';
+import React from "react";
+import StoryList from "../components/StoryList";
+import StoryForm from "../components/StoryForm";
+import FriendList from "../components/FriendList";
 
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { QUERY_STORIES, QUERY_ME_BASIC } from '../utils/queries';
+import "./Home.css";
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_STORIES, QUERY_ME_BASIC } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_STORIES);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const stories = data?.stories || [];
-
 
   const loggedIn = Auth.loggedIn();
 
@@ -23,15 +23,16 @@ const Home = () => {
             <StoryForm />
           </div>
         )}
-        <div className={`flex-row justify-center mb-3 ${loggedIn && 'col-lg-8'}`}>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <StoryList
-              stories={stories}
-              title="Explore Some Stories"
-            />
-          )}
+        <div className="home-support-header">
+          <div
+            className={`flex-row justify-center mb-3 ${loggedIn && "col-lg-8"}`}
+          >
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <StoryList stories={stories} title="Explore Some Stories" />
+            )}
+          </div>
         </div>
         {loggedIn && userData ? (
           <div className="col-4 col-lg-3 ml-3">
--- client/src/pages/SingleStory.js
+++ client/src/pages/SingleStory.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
-import Like from '../components/Like'
+import CommentList from "../components/CommentList";
+import CommentForm from "../components/CommentForm";
+import Like from "../components/Like";
+import "./SingleStory.css";
 
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { QUERY_STORY } from '../utils/queries';
-import { QUERY_ME_BASIC } from '../utils/queries';
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_STORY } from "../utils/queries";
+import { QUERY_ME_BASIC } from "../utils/queries";
 
 const SingleStory = (props) => {
   const { id: storyId } = useParams();
@@ -17,15 +18,15 @@ const SingleStory = (props) => {
     variables: { id: storyId },
   });
 
-  console.log(useQuery(QUERY_STORY, {
-    variables: { id: storyId },
-  }))
-
-
+  console.log(
+    useQuery(QUERY_STORY, {
+      variables: { id: storyId },
+    })
+  );
 
   const { data: userData } = useQuery(QUERY_ME_BASIC);
 
-  console.log(data)
+  console.log(data);
   const story = data?.story || {};
   // console.log(story.likeCount)
   // console.log(story)
@@ -35,36 +36,43 @@ const SingleStory = (props) => {
   }
 
   return (
-    <div className="flex-column">
-      <div className="card mb-3">
-        <p className="card-header">
+    <div class="story-container">
+      <div class="card mb-3">
+        <p className="">
           <span style={{ fontWeight: 700 }} className="text-light">
             {story.username}
-          </span>{' '}
-          story posted on {story.createdAt}
+          </span>{" "}
+          Story posted on {story.createdAt}
         </p>
-        <div className="card-body">
-          <p>{story.storyTitle}</p>
-          <p>{story.storyText}</p>
+
+        <div className="bar">
+          <div class="emptybar">{story.storyTitle}</div>
+          <div class="filledbar">{story.storyText}</div>
+        </div>
+        <div class="circle">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <circle class="stroke" cx="60" cy="60" r="50" />
+          </svg>
         </div>
       </div>
+
       <div className="mb-3">
         {Auth.loggedIn() ? (
-              <>
-              </>
-            ) : (
-              <>
-<p className="dgreen text-italic text-right">Login to share your stories and make friends!</p> 
-              </>
-            )}
-        
-        {Auth.loggedIn() && <Like storyId={story._id} username = {userData.me.username} />}
-        
+          <></>
+        ) : (
+          <>
+            <p className="dgreen text-italic text-right">
+              Login to share your stories and make friends!
+            </p>
+          </>
+        )}
+
+        {Auth.loggedIn() && (
+          <Like storyId={story._id} username={userData.me.username} />
+        )}
       </div>
 
-      {story.commentCount > 0 && (
-        <CommentList comments={story.comments} />
-      )}
+      {story.commentCount > 0 && <CommentList comments={story.comments} />}
 
       {Auth.loggedIn() && <CommentForm storyId={story._id} />}
     </div>
